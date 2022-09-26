70c8101f20adf303b4022d1c79f67d51e65fa19a
YutoFunaki <104195830+YutoFunaki@users.noreply.github.com>
2022-09-26 22:42:56 +0900

    Initial Commit

---
+++ practice/practiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  practiceApp.swift
+//  practice
+//
+//  Created by 船木勇斗 on 2022/09/26.
+//
+
+import SwiftUI
+
+@main
+struct practiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
